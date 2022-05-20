25fc63b3c5d7f776db2ad37bed64772af04ae1bd
Ilya <ilya@sinyugin.ru>
2022-05-20 15:02:56 +0200

    deploy js init

--- contracts/TinderERC721.sol
+++ contracts/TinderERC721.sol
@@ -5,9 +5,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-contract TinderERC721 is
-    ERC721URIStorage //class Tinder extends ..
-{
+//class Tinder extends ..
+contract TinderERC721 is ERC721URIStorage {
     uint256 TINDER_TOKEN_ID;
 
     constructor() ERC721("CleverNFT", "CN") {}
